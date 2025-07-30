3e730d76e866540e828d444b4edf4d5802f1813d
hayatroid <hayatroid@gmail.com>
2025-07-30 20:56:17 +0900

    🗿 add doc comments & `cargo readme > README.md`

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # hayatmoji-cli
+
 A hayatmoji interactive cli tool for using emojis on commits. 🤦
+
+## Install
+
+### cargo
+
+```bash
+cargo install hayatmoji-cli
+```
+
+## Usage
+
+```bash
+hayatmoji --help
+```
+
+```
+A hayatmoji client for using emojis on commit messages.
+
+Usage: hayatmoji [OPTIONS]
+
+Options:
+  -c, --commit  Interactively commit using the prompts
+  -h, --help    Print help
+```
+
+### Commit
+
+Start the interactive commit client, to auto generate your commit based on your prompts.
+
+```bash
+hayatmoji -c
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
-//! # hayatmoji-cli
 //! A hayatmoji interactive cli tool for using emojis on commits. 🤦
+//!
+//! # Install
+//!
+//! ## cargo
+//!
+//! ```bash
+//! cargo install hayatmoji-cli
+//! ```
+//!
+//! # Usage
+//!
+//! ```bash
+//! hayatmoji --help
+//! ```
+//!
+//! ```text
+//! A hayatmoji client for using emojis on commit messages.
+//!
+//! Usage: hayatmoji [OPTIONS]
+//!
+//! Options:
+//!   -c, --commit  Interactively commit using the prompts
+//!   -h, --help    Print help
+//! ```
+//!
+//! ## Commit
+//!
+//! Start the interactive commit client, to auto generate your commit based on your prompts.
+//!
+//! ```bash
+//! hayatmoji -c
+//! ```
 
 use std::{error::Error, fmt::Display};
 
